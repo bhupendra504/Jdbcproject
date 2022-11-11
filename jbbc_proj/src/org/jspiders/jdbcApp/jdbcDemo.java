38f84093e5c12978498ec4e11662948748963725
bhupendra504 <bhupendra1998patel@gmail.com>
2022-11-11 18:01:51 +0530

    update

--- jbbc_proj/src/org/jspiders/jdbcApp/jdbcDemo.java
+++ jbbc_proj/src/org/jspiders/jdbcApp/jdbcDemo.java
@@ -6,11 +6,14 @@ public class jdbcDemo {
 	public static void main(String args[])
 	{
 		Connection con=null;
+		Statement stmt=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Drive class loaded&Registered");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");
 			System.out.println("Connection Established With DataBase Server");
+			stmt=con.createStatement();
+			System.out.println("Platform Created");
 		}
 		catch(ClassNotFoundException | SQLException e)
 		{
@@ -18,6 +21,17 @@ public class jdbcDemo {
 		}
 		finally
 		{
+			if(stmt!=null)
+			{
+				try
+				{
+					stmt.close();
+				}
+				catch(SQLException e)
+				{
+					e.printStackTrace();
+				}
+			}
 			if(con!=null)
 			{
 				try
